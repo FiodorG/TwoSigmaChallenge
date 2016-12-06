3b7206f165331d5beee5208806afeed723dcaa54
FiodorG <fedor.gorokhovik@gmail.com>
2016-12-06 01:07:03 +0000

    add check on id

--- Scratch.py
+++ Scratch.py
@@ -10,7 +10,7 @@ def plot_column(dataset, column, index_start=0, index_end=-1):
 
     if index_end != -1:
         values = np.cumsum(dataset[column][index_start:index_end])
-        times = np.arange(index_end - index_start)/750/365
+        times = np.arange(index_end - index_start)
     else:
         values = dataset[column][index_start:]
         times = np.arange(len(values) - index_start)
@@ -70,12 +70,25 @@ columns_derived = [c for c in dataset.columns if 'derived' in c] + ['timestamp']
 columns_fundamental = [c for c in dataset.columns if 'fundamental' in c] + ['timestamp']
 columns_technical = [c for c in dataset.columns if 'technical' in c] + ['timestamp']
 
-#plot_column(dataset, 'y', 0, 1500000)
+#plot_column(dataset, 'id', 0, 1000)
 
 df = dataset[['timestamp', 'y']].groupby('timestamp').agg([np.mean, np.std, len]).reset_index()
-df_derived = dataset[columns_derived].groupby('timestamp').agg([np.mean]).reset_index()
+df_derived = dataset[columns_derived].groupby('timestamp').agg([np.mean, np.std, len]).reset_index()
 #df_fundamental = dataset[columns_fundamental].groupby('timestamp').agg([np.mean, np.std, len]).reset_index()
 #df_technical = dataset[columns_technical].groupby('timestamp').agg([np.mean, np.std, len]).reset_index()
+
+df2 = dataset[['id', 'timestamp', 'y']]
+temp = df2.groupby('id').apply(len).sort_values(ascending=False).reset_index()
+print(temp.head(5))
+
+temp2 = df2[df2['id'].isin(temp['id'][1:2].values)]
+temp2 = temp2[['timestamp', 'y']]
+print(temp2)
+plt.figure(figsize=(8, 4))
+plt.plot(temp2['timestamp'], np.cumsum(temp2['y']))
+plt.xlabel('timestamp')
+plt.ylabel('y')
+plt.show()
 
 #yMean = df1['y']['mean']
 #yMean = dataset['y'].head(n=1000)
@@ -83,11 +96,11 @@ df_derived = dataset[columns_derived].groupby('timestamp').agg([np.mean]).reset_
 #autocorrelation_plot(yMean)
 
 #plot_all_graphs(df_derived)
-yMean = df['y']['mean']
-corrColumns = [c for c in df_derived.columns if 'timestamp' not in c]
-derived = np.array([df_derived[c] for c in corrColumns])
-Corr = np.corrcoef(yMean, derived)
-sns.clustermap(pd.DataFrame(np.abs(Corr), columns=['---y--']+[c[0].split('_')[1] for c in corrColumns]))
+#yMean = df['y']['mean']
+#corrColumns = [c for c in df_derived.columns if 'timestamp' not in c]
+#derived = np.array([df_derived[c] for c in corrColumns])
+#Corr = np.corrcoef(yMean, derived)
+#sns.clustermap(pd.DataFrame(np.abs(Corr), columns=['---y--']+[c[0].split('_')[1] for c in corrColumns]))
 
-plt.show()
+#plt.show()
 print()
