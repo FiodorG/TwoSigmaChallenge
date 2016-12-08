abf8829d30d1a26a3a3740f1086fb2549e05c9ae
gorokf <fedor.gorokhovik@gmail.com>
2016-12-08 01:03:46 +0000

    Add PCA and some other visualization stuff

--- Scratch.py
+++ Scratch.py
@@ -1,99 +1,42 @@
 from __future__ import print_function
 import numpy as np
 import pandas as pd
+import Visualization
 import matplotlib.pyplot as plt
 import seaborn as sns
-from pandas.tools.plotting import autocorrelation_plot
-
-
-def plot_column(dataset, column, index_start=0, index_end=-1):
-
-    if index_end != -1:
-        values = np.cumsum(dataset[column][index_start:index_end])
-        times = np.arange(index_end - index_start)
-    else:
-        values = dataset[column][index_start:]
-        times = np.arange(len(values) - index_start)
-
-    plt.plot(times, values)
-    plt.xlabel(column)
-    axes = plt.gca()
-    axes.set_xlim([0, times[-1]])
-    plt.show()
-
-
-def show_y_stats(dataset):
-    n = dataset['timestamp']
-    y_mean = dataset['y']['mean']
-    y_std = dataset['y']['std']
-
-    plt.plot(n, y_mean, '.')
-    plt.xlabel('n')
-    plt.ylabel('$y_{n}^{Mean}$')
-    plt.show()
-    plt.savefig('yMean.png')
-
-    plt.plot(n, y_std, '.')
-    plt.xlabel('n')
-    plt.ylabel('$y_{n}^{Std}$')
-    plt.show()
-    plt.savefig('yStd.png')
-
-
-def plot_all_graphs(df):
-    i = 0
-    n = 5
-    columns = df.columns
-    while True:
-        if i >= len(columns):
-            break
-        for j in range(n):
-            if i >= len(columns):
-                break
-
-            if j == 0:
-                plt.figure(figsize=(8, 8.0 / n))
-
-            plt.axes([j * 1.0 / n, 0, 1.0 / n, 1])
-            plt.plot(df['timestamp'], df[columns[i]])
-            plt.xticks([])
-            plt.yticks([])
-            i += 1
-    plt.show()
+from sklearn.decomposition import PCA
 
 
 with pd.HDFStore("train.h5", "r") as data_file:
     dataset = data_file.get("train")
 
 column_all = dataset.columns
-columns_derived = [c for c in dataset.columns if 'derived' in c] + ['timestamp']
-columns_fundamental = [c for c in dataset.columns if 'fundamental' in c] + ['timestamp']
-columns_technical = [c for c in dataset.columns if 'technical' in c] + ['timestamp']
+columns_derived = [c for c in dataset.columns if 'derived' in c] + ['timestamp', 'id']
+columns_fundamental = [c for c in dataset.columns if 'fundamental' in c] + ['timestamp', 'id']
+columns_technical = [c for c in dataset.columns if 'technical' in c] + ['timestamp', 'id']
 
-#plot_column(dataset, 'id', 0, 1000)
+df_y = dataset[['id', 'timestamp', 'y']]
+# df_derived = dataset[columns_derived].groupby('timestamp')
+# df_fundamental = dataset[columns_fundamental].groupby('timestamp').agg([np.mean, np.std, len]).reset_index()
+# df_technical = dataset[columns_technical].groupby('timestamp').agg([np.mean, np.std, len]).reset_index()
 
-df = dataset[['timestamp', 'y']].groupby('timestamp').agg([np.mean, np.std, len]).reset_index()
-df_derived = dataset[columns_derived].groupby('timestamp').agg([np.mean, np.std, len]).reset_index()
-#df_fundamental = dataset[columns_fundamental].groupby('timestamp').agg([np.mean, np.std, len]).reset_index()
-#df_technical = dataset[columns_technical].groupby('timestamp').agg([np.mean, np.std, len]).reset_index()
+Visualization.plot_pca(dataset, 'y')
 
-df2 = dataset[['id', 'timestamp', 'y']]
-temp = df2.groupby('id').apply(len).sort_values(ascending=False).reset_index()
-print(temp.head(5))
+# temp = df_y.apply(len).sort_values(ascending=False).reset_index()
+# print(temp.head(5))
+# temp2 = df_y[df_y['id'].isin(temp['id'][1:2].values)]
+# temp2 = temp2[['timestamp', 'y']]
+# print(temp2)
+# plt.figure(figsize=(8, 4))
+# plt.plot(temp2['timestamp'], np.cumsum(temp2['y']))
+# plt.xlabel('timestamp')
+# plt.ylabel('y')
+# plt.show()
 
-temp2 = df2[df2['id'].isin(temp['id'][1:2].values)]
-temp2 = temp2[['timestamp', 'y']]
-print(temp2)
-plt.figure(figsize=(8, 4))
-plt.plot(temp2['timestamp'], np.cumsum(temp2['y']))
-plt.xlabel('timestamp')
-plt.ylabel('y')
-plt.show()
+# Visualization.plot_column_per_id_and_timestamp(dataset, 'derived_3')
+# Visualization.plot_column(dataset, 'y', 800, True)
 
-#yMean = df1['y']['mean']
-#yMean = dataset['y'].head(n=1000)
-#plt.figure()
-#autocorrelation_plot(yMean)
+
 
 #plot_all_graphs(df_derived)
 #yMean = df['y']['mean']
@@ -102,5 +45,8 @@ plt.show()
 #Corr = np.corrcoef(yMean, derived)
 #sns.clustermap(pd.DataFrame(np.abs(Corr), columns=['---y--']+[c[0].split('_')[1] for c in corrColumns]))
 
-#plt.show()
+plt.show()
+print(columns_derived)
+print(columns_fundamental)
+print(columns_technical)
 print()
---
+++ Visualization.py
@@ -0,0 +1,135 @@
+from __future__ import print_function
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas.tools.plotting as pdtools
+import sklearn.preprocessing as skpre
+from sklearn.decomposition import PCA
+
+def plot_column(dataset, column, id, cumsum=False, index_start=0, index_end=-1):
+    dataset = dataset[dataset['id'] == id]
+
+    if index_end != -1:
+        values = np.cumsum(dataset[column][index_start:index_end])
+        times = np.arange(index_end - index_start)
+    else:
+        values = dataset[column][index_start:]
+        times = np.arange(len(values) - index_start)
+
+    if cumsum:
+        values = np.cumsum(values)
+
+    plt.plot(times, values)
+    plt.xlabel(column)
+    axes = plt.gca()
+    axes.set_xlim([0, times[-1]])
+    plt.show()
+
+
+def show_y_stats(dataset):
+    n = dataset['timestamp']
+    y_mean = dataset['y']['mean']
+    y_std = dataset['y']['std']
+
+    plt.plot(n, y_mean, '.')
+    plt.xlabel('n')
+    plt.ylabel('$y_{n}^{Mean}$')
+    plt.show()
+    plt.savefig('yMean.png')
+
+    plt.plot(n, y_std, '.')
+    plt.xlabel('n')
+    plt.ylabel('$y_{n}^{Std}$')
+    plt.show()
+    plt.savefig('yStd.png')
+
+
+def plot_all_graphs(df):
+    i = 0
+    n = 5
+    columns = df.columns
+    while True:
+        if i >= len(columns):
+            break
+        for j in range(n):
+            if i >= len(columns):
+                break
+
+            if j == 0:
+                plt.figure(figsize=(8, 8.0 / n))
+
+            plt.axes([j * 1.0 / n, 0, 1.0 / n, 1])
+            plt.plot(df['timestamp'], df[columns[i]])
+            plt.xticks([])
+            plt.yticks([])
+            i += 1
+    plt.show()
+    
+    
+def plot_column_per_id_and_timestamp(dataset, column):
+
+    instr_time = dataset.groupby('id').agg({'timestamp': [np.min, np.max]})
+    sort_order = instr_time.sort_values(by=[('timestamp', 'amin'), ('timestamp', 'amax')]).index
+    # df[column] += 0.2
+    df1 = dataset.set_index(['id', 'timestamp']).unstack('id').reindex(sort_order)
+    np_array = df1.fillna(0).values
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.imshow(np_array, cmap='gray')
+    plt.show()
+
+
+def plot_autocorrelation(dataset, column, id):
+    dataset = dataset[['id', 'timestamp', column]]
+    pdtools.autocorrelation_plot(dataset[dataset.id == id])
+
+
+def plot_pca(dataset, column):
+    dataset = dataset[['id', 'timestamp', column]]
+    df = dataset.set_index(['id', 'timestamp']).unstack('id').fillna(0)
+    dim = df.shape
+    # print(df.head())
+    # print(dim)
+    n = 1000
+    pca = PCA(n_components=n, whiten=True)
+    pca.fit(df)
+
+    plt.figure(1)
+    plt.subplot(511)
+    plt.bar(np.arange(n), pca.explained_variance_ratio_, alpha=0.5, align='center', label='individual explained variance')
+    plt.step(np.arange(n), np.cumsum(pca.explained_variance_ratio_), where='mid', label='cumulative explained variance')
+    plt.xlabel('Number of chosen ids')
+    plt.ylabel('Cumulative explained variance')
+    plt.xlim([0, n])
+
+    pca2 = PCA(n_components=2, whiten=True)
+    reduced_data = pca2.fit_transform(df)
+
+    plt.subplot(512)
+    plt.plot(reduced_data[:, 0], 'r')
+    plt.ylabel('PC1')
+    plt.xlabel('timestamp')
+    plt.xlim([0, dim[0]])
+
+    plt.subplot(513)
+    plt.plot(reduced_data[:, 1], 'g')
+    plt.ylabel('PC2')
+    plt.xlabel('timestamp')
+    plt.xlim([0, dim[0]])
+
+    plt.subplot(514)
+    plt.bar(np.arange(dim[1]), pca.components_[0, :])
+    plt.ylabel('weight of asset id in PC1')
+    plt.xlabel('id')
+    plt.xlim([0, dim[1]])
+
+    plt.subplot(515)
+    plt.bar(np.arange(dim[1]), pca.components_[1, :])
+    plt.ylabel('weight of asset id in PC2')
+    plt.xlabel('id')
+    plt.xlim([0, dim[1]])
+
+    plt.show()
+    print()
+
